ae1e13f4795dbd42b20dd9b49373bc4c27804cf6
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-01-03 15:55:15 +0530

    Delete post

--- src/app.js
+++ src/app.js
@@ -1,57 +1,65 @@
-import { http } from './http';
-import { ui } from './ui';
+import  {http} from './http';
+import {ui} from './ui';
 
-// Get posts on DOM load
-document.addEventListener('DOMContentLoaded', getPosts);
+    ///get Posts
+    document.addEventListener('DOMContentLoaded',getPosts);
+    ///post submit event///
+    document.querySelector('.post-submit').addEventListener('click',submitPost);
+    //delete post//
+    document.querySelector('#posts').addEventListener('click',deletePost);
 
-// Listen for add post
-document.querySelector('.post-submit').addEventListener('click', submitPost);
-
-// Listen for delete
-document.querySelector('#posts').addEventListener('click', deletePost);
-
-// Get Posts
-function getPosts() {
-  http
-    .get('http://localhost:3000/posts')
-    .then(data => ui.showPosts(data))
-    .catch(err => console.log(err));
+ 
+//get Post from API
+function getPosts(){
+    http.get('http://localhost:3000/posts')
+    .then(data=>ui.showPosts(data))
+    .catch(error=>console.log(error))
 }
 
-// Submit Post
-function submitPost() {
-  const title = document.querySelector('#title').value;
-  const body = document.querySelector('#body').value;
-
-  const data = {
-    title,
-    body
-  };
-
-  // Create Post
-  http
-    .post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
-}
-
-// Delete Post
-function deletePost(e) {
-  e.preventDefault();
-  if (e.target.parentElement.classList.contains('delete')) {
-    const id = e.target.parentElement.dataset.id;
-    if (confirm('Are you sure?')) {
-      http
-        .delete(`http://localhost:3000/posts/${id}`)
-        .then(data => {
-          ui.showAlert('Post Removed', 'alert alert-success');
-          getPosts();
-        })
-        .catch(err => console.log(err));
+//Submit Input Post
+function submitPost(e){
+    //Input from User
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+    
+    //Input data
+    const data = {
+        title,
+        body
     }
-  }
+
+    http.post('http://localhost:3000/posts',data)
+        .then(data=>{
+            getPosts();
+            ui.clearFields();
+            ui.showAlert('Post Added','alert alert-success');
+        })
+        .catch(err=>console.log(err));
+
+
 }
+
+function deletePost(e){
+    const id = e.target.parentElement.dataset.id;
+
+   if(e.target.parentElement.classList.contains('delete')){
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you Sure ?')){
+        http.delete(`http://localhost:3000/posts/${id}`)
+            .then(data=>{
+                getPosts();
+                ui.showAlert('Post Removed','alert alert-success');
+            })
+            .catch(err=>{
+                console.log(err)
+            });
+    }
+   }
+}
+  
+
+
+
+
+
+
